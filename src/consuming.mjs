26d19ee64fb0dbcb2088c8dd0e6fee4370ba9eb3
sanyaodare@gmail.com <sanyaodare@gmail.com>
2020-10-09 17:29:45 +0100

    handle error with promises

--- src/consuming.mjs
+++ src/consuming.mjs
@@ -1,10 +1,16 @@
 import setText, {appendText, showWaiting, hideWaiting} from "./results.mjs";
 
 export function get() {
-  axios.get("http://localhost:3000/orders/1");
+  axios.get("http://localhost:3000/orders/1").then(({ data }) => {
+    setText(JSON.stringify(data));
+  });
 }
 
 export function getCatch() {
+  axios.get("http://localhost:3000/orders/123").then(result => {
+    setText(JSON.stringify(result.data));  
+  })
+  .catch(err => setText(err));
 }
 
 export function chain() {
@@ -12,6 +18,6 @@ export function chain() {
 
 export function chainCatch() {
 }
-
+ 
 export function final() {
 }
