938fa7ecc32fd26b77b48d161f7fc94a867a3999
sanyaodare@gmail.com <sanyaodare@gmail.com>
2020-10-09 19:04:15 +0100

    catch error in a chain

--- src/consuming.mjs
+++ src/consuming.mjs
@@ -7,17 +7,40 @@ export function get() {
 }
 
 export function getCatch() {
-  axios.get("http://localhost:3000/orders/123").then(result => {
+  axios.get(
+    "http://localhost:3000/orders/123"
+    ).then (result => {
     setText(JSON.stringify(result.data));  
   })
   .catch(err => setText(err));
 }
 
 export function chain() {
+  axios
+  .get("http://localhost:3000/orders/1")
+  .then(({ data }) => {
+    return axios.get(
+      `http://localhost:3000/addresses/${data.shippingAddress}`
+      );
+  })
+  .then(({data}) => {
+    setText(`City: ${data.city}`);
+  });
 }
 
 export function chainCatch() {
+  axios
+  .get("http://localhost:3000/orders/1")
+  .then(({ data }) => {
+     return axios.get(
+       `http://localhost:3000/addresses/${data.shippingAddress}`
+    );
+  })
+  .then(({ data }) => {
+    setText(`City: ${data.my.city}`);
+  })
+  .catch(err => setText(err));
 }
- 
+
 export function final() {
 }
